b95450ae12977b20a91682b497d8e4bf991aff96
Radoslav Naydenov <radoales@users.noreply.github.com>
2020-06-08 10:05:46 +0200

    Add Selenium for UI Test

---
+++ SomeWebShowroom.Test/UITests/AutomatedUITest.cs
@@ -0,0 +1,36 @@
+﻿namespace SomeWebShowroom.Test.UITests
+{
+    using FluentAssertions;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using System;
+    using Xunit;
+
+    public class AutomatedUITest : IDisposable
+    {
+        private readonly IWebDriver driver;
+        public AutomatedUITest()
+        {
+            this.driver = new ChromeDriver();
+        }
+
+        public void Dispose()
+        {
+            this.driver.Quit();
+            this.driver.Dispose();
+        }
+
+        [Fact]
+        public void NavigateToHome_Shouldopen_HomePage()
+        {
+           this. driver.Navigate()
+                .GoToUrl("https://localhost:44393");
+
+            var result = this.driver.Title;
+
+            result
+                .Should()
+                .Be("Home Page");
+        }
+    }
+}
