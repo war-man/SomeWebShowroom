fdc55de7eedd0808569c3c637b269e82ad2acdee
Radoslav Naydenov <radoales@users.noreply.github.com>
2020-06-08 18:29:22 +0200

    Add TestData for UiTest

--- SomeWebShowroom.Test/UITests/AutomatedUITest.cs
+++ SomeWebShowroom.Test/UITests/AutomatedUITest.cs
@@ -3,12 +3,14 @@
     using FluentAssertions;
     using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Support.PageObjects;
     using System;
     using Xunit;
 
     public class AutomatedUITest : IDisposable
     {
         private readonly IWebDriver driver;
+
         public AutomatedUITest()
         {
             this.driver = new ChromeDriver();
@@ -20,17 +22,42 @@
             this.driver.Dispose();
         }
 
-        [Fact]
-        public void NavigateToHome_Shouldopen_HomePage()
+        [Theory]
+        [ClassData(typeof(TestData.NavigateToPageTestData))]
+        public void NavigateToPage_Shouldopen_TheRightPage(string url, string title)
         {
+            //Arrange
            this. driver.Navigate()
-                .GoToUrl("https://localhost:44393");
+                .GoToUrl("https://localhost:44393/" + url);
 
+            //Act
             var result = this.driver.Title;
 
+            //Assert
             result
                 .Should()
-                .Be("Home Page");
+                .Be(title);
         }
+
+
+        //[Fact]
+        //public void Create_WhenSuccessfullyExecuted_ReturnsIndexViewWithNewEmployee()
+        //{
+        //    this.driver.Navigate()
+        //        .GoToUrl("https://localhost:44393/Identity/register");
+
+        //    this.driver.FindElement(By.Id("Email"))
+        //        .SendKeys("");
+        //    this.driver.FindElement(By.Id("Username"))
+        //        .SendKeys("admin");
+
+        //    this.driver.FindElement(By.Id("submit"))
+        //        .Click();
+
+        //    // Assert.Equal("Register", this.driver.Title);
+        //    // Assert.Contains("Email is required", this.driver.PageSource);
+        //    var result = driver.FindElement(By.Id("Email")).GetAttribute("Required").
+        //    Assert.Equal("Email is required", result);
+        //}
     }
 }
---
+++ SomeWebShowroom.Test/UITests/TestData/NavigateToPageTestData.cs
@@ -0,0 +1,17 @@
+﻿namespace SomeWebShowroom.Test.UITests.TestData
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    public class NavigateToPageTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { "", "Home Page"};
+            yield return new object[] { "Identity/register", "Register"};
+            yield return new object[] { "Identity/login", "Login" };
+            yield return new object[] { "products", "Browse Products" };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
